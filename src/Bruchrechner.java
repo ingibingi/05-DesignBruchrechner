ca51bda1bcd848aa5a138067c02edef3af510261
Ingo Schlapschy <ingo.schlapschy@gmail.com>
2024-10-01 08:53:02 +0200

    Add Design

---
+++ src/Bruchrechner.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class Bruchrechner {
+    private JTextField textField1;
+    private JTextField txtNenner1;
+    private JTextField txtZaehler2;
+    private JTextField txtNenner2;
+    private JComboBox cbOperation;
+    private JLabel lblErgebnis;
+    private JSeparator txtZaehler1;
+    private JButton btnEquals;
+    private JTextPane txtHInweise;
+    private JButton resetButton;
+}
